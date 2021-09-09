2e74f6588ae5d3888c1ba3ed6835508c213a9ed9
danmed <danmed@gmail.com>
2021-09-09 14:04:26 +0100

    Update consumption.sh

--- consumption.sh
+++ consumption.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+consumption=0
 since_midnight=$(( $(date "+10#%H * 60 + 10#%M") ))
-/root/glow2mqtt/glowmarkt-csv  -u '<USERNAME>' -p '<PASSWORD>' -m $since_midnight -d P1W -c electricity.consumption > consumption.csv
+/root/glow2mqtt/glowmarkt-csv  -u '<USERNAME>' -p '<PASSWORD>' -m $since_midnight -d PT30M -c electricity.consumption -n > consumption.csv
+#consumption=`glowmarkt-today -u '<USERNAME>' -p '<PASSWORD>' -c electricity.consumption
+
 while read line
 do
-   output=${line//$'\n'/}
-   consumption=$(expr $consumption + $output)
-done < <(tail -n +2 consumption.csv)
-
-mosquitto_pub -h <MQTT IP> -m $consumption -t glowmarkt/consumptiontoday -u <MQTT_USER> -P <MQTT_PASS> -r -d
+   output=${line//$'\r'/}
+   consumption=$(bc <<< "$consumption + $output")
+done < <(tail -n+1 consumption.csv)
+mosquitto_pub -h <MQTT_IP> -m $consumption -t glowmarkt/consumptiontoday -u <MQTT_USER> -P <MQTT_PASS> -r -d
