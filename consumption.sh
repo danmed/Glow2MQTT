0ad1a76f5e2ec5a5158385816135039356b99ce8
danmed <danmed@gmail.com>
2021-09-10 22:47:50 +0100

    Convert cost output to £, add comments

--- consumption.sh
+++ consumption.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+####CONFIG#####
 glowuser="<CHANGEME>"
 glowpass="<CHANGEME>"
 mqttip="<CHANGEME>"
 mqttuser="<CHANGEME>"
 mqttpass="<CHANGEME>"
-consumption=`glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption`
-cost=`glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption.cost`
+################
+
+####GRAB VALUES FROM DCC####
+consumption=`/usr/local/bin/glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption`
+cost=`/usr/local/bin/glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption.cost`
+
+####FORMAT COST TO BE IN £####
+cost=$(echo "scale=10; $cost/100" | bc -l)
+
+###PUBLISH TO MQTT####
 mosquitto_pub -h $mqttip -m $consumption -t glowmarkt/consumptiontoday -u $mqttuser -P $mqttpass -r -d
 mosquitto_pub -h $mqttip -m $cost -t glowmarkt/costtoday -u $mqttuser -P $mqttpass -r -d
