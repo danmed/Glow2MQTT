2e973efc1707df8b1fa4ee0c717cb8ea7a1d35ee
danmed <danmed@gmail.com>
2021-09-17 00:56:59 +0100

    Add Gas Readings

--- consumption.sh
+++ consumption.sh
@@ -3,12 +3,20 @@
 glowuser="<CHANGEME>"
 glowpass="<CHANGEME>"
 mqttip="<CHANGEME>"
-mqtttopic_consumption="glowmarkt/consumptiontoday"
-mqtttopic_cost="glowmarkt/costtoday"
+mqtttopic_elec_consumption="glowmarkt/elecconsumptiontoday"
+mqtttopic_elec_cost="glowmarkt/eleccosttoday"
+mqtttopic_gas_consumption="glowmarkt/gasconsumptiontoday"
+mqtttopic_gas_cost="glowmarkt/gascosttoday"
 mqttuser="<CHANGEME>"
 mqttpass="<CHANGEME>"
 ################
 
-####GRAB VALUES FROM DCC####
-consumption=`consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption -t $mqtttopic_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
-cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption.cost -t $mqtttopic_cost --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+#IF YOU DONT HAVE / WANT GAS THEN HASH OUT THE GAS SECTION
+
+####ELECTRIC####
+consumption=`consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption -t $mqtttopic_elec_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption.cost -t $mqtttopic_elec_cost --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+
+####GAS####
+consumption=`consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption -t $mqtttopic_gas_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption.cost -t $mqtttopic_gas_cost --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
