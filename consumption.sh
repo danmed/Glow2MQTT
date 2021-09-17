1ccc7e7c12e56e2f24f75950dd333d454d20749f
danmed <danmed@gmail.com>
2021-09-17 01:22:37 +0100

    Fixing fat finger copy pasting

--- consumption.sh
+++ consumption.sh
@@ -18,11 +18,11 @@ mqttpass="<CHANGEME>"
 #IF YOU DONT HAVE / WANT GAS THEN HASH OUT THE GAS SECTION
 
 ####ELECTRIC####
-consumption=`consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption -t $mqtttopic_elec_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption -t $mqtttopic_elec_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
 cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.consumption.cost -t $mqtttopic_elec_cost --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
 
 ####GAS####
-consumption=`consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption -t $mqtttopic_gas_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption -t $mqtttopic_gas_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
 cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption.cost -t $mqtttopic_gas_cost --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
 
 ###OPTIONAL LOGGING OF DATA###
