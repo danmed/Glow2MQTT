c122bc0c2f1e424a62bd7c5e2fb9a67e687ceab7
danmed <danmed@gmail.com>
2021-09-17 01:05:14 +0100

    Add CSV logging.

--- consumption.sh
+++ consumption.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+dtfile=$(date '+%d-%m-%Y');
+
 ####CONFIG#####
 glowuser="<CHANGEME>"
 glowpass="<CHANGEME>"
@@ -20,3 +24,19 @@ cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c electricity.con
 ####GAS####
 consumption=`consumption=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption -t $mqtttopic_gas_consumption --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
 cost=`/usr/local/bin/glowmarkt-mqtt -u $glowuser -p $glowpass -c gas.consumption.cost -t $mqtttopic_gas_cost --host $mqttip --mqtt-username $mqttuser --mqtt-pass $mqttpass`
+
+###OPTIONAL LOGGING OF DATA###
+### If you un hash the below lines it will create dated csv files in a directory called output###
+### Change paths as required###
+###GATHER###
+#elec_consumption=`/usr/local/bin/glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption`
+#elec_cost=`/usr/local/bin/glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption.cost`
+#gas_consumption=`/usr/local/bin/glowmarkt-today -u $glowuser -p $glowpass -c gas.consumption`
+#gas_cost=`/usr/local/bin/glowmarkt-today -u $glowuser -p $glowpass -c gas.consumption.cost`
+
+###LOG##
+#echo $dt $elec_consumption >> /root/glow2mqtt/output/$dtfile-elec_consumption.csv
+#echo $dt $elec_cost >> /root/glow2mqtt/output/$dtfile-elec_cost.csv
+#echo $dt $gas_consumption >> /root/glow2mqtt/output/$dtfile-gas_consumption.csv
+#echo $dt $gas_cost >> /root/glow2mqtt/output/$dtfile-gas_cost.csv
+
